65fb8d98e4b17b45cea0d3cc61b8a22c484df463
maheshy6 <maheshy675@gmail.com>
2024-12-21 00:09:37 +0530

    b4

---
+++ TravelItineraryPlanning/Backend/model/itinerary.model.js
@@ -0,0 +1,13 @@
+import { Schema,model } from "mongoose";
+const tripdata = new Schema({
+    // username:{ type: Schema.Types.ObjectId, ref: "user", required: true },
+    username:{ type: String, required: true },
+    email:{type:String ,required:true},
+    destination:{type:String ,required:true},
+    startDate:{type:String,required:true},
+    endDate:{type:String ,default:""},
+    budget:{type:Number ,default:""}
+})
+
+const Trip = model("trip",tripdata)
+export default Trip
